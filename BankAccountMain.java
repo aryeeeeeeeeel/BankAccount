edec42eaaa4918f23e40dec74a74391142a66a2c
aryeeeeeeeeel <arielsumantin69@gmail.com>
2023-10-10 16:34:40 +0800

    Bank Account

---
+++ BankAccountMain.java
@@ -0,0 +1,14 @@
+class BankAccountMain {
+    public static void main(String[] args){
+                
+        BankAccount account = new BankAccount();
+        
+        account.setName("Ariel Sumantin");
+        account.setAge(21);
+        account.setAccountNumber(20211276);
+        account.deposit(61000.0);
+        account.deposit(7000.0);
+        account.displaySummary();
+        
+    }
+}
